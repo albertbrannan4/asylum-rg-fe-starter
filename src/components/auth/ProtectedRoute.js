62e966f9f337090a25de64436afce47da15943ad
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-05-12 11:14:52 -0400

    Auth0 Set Up

---
+++ src/components/auth/ProtectedRoute.js
@@ -0,0 +1,8 @@
+import { withAuthenticationRequired } from '@auth0/auth0-react';
+import { Route } from 'react-router-dom';
+
+const ProtectedRoute = ({ component, ...args }) => (
+  <Route component={withAuthenticationRequired(component)} {...args} />
+);
+
+export default ProtectedRoute;

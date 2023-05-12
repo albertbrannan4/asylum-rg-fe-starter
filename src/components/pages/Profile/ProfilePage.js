708cb7e429e27d74cc14724feb46f39ca50e6e49
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-05-12 15:11:02 -0400

    Profile wip

--- src/components/pages/Profile/ProfilePage.js
+++ src/components/pages/Profile/ProfilePage.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import Profile from './Profile';
-
+import { useAuth0 } from '@auth0/auth0-react';
 const ProfilePage = () => {
-  return <Profile />;
+  const { user } = useAuth0();
+  const card = {
+    border: '1px solid red',
+    width: '25%',
+    height: '40vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: '4%',
+    borderRadius: '10px',
+  };
+  return (
+    <div>
+      <div style={card}>
+        <img style={{ width: '40%' }} src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;

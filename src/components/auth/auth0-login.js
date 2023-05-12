4b44a89328701f90ecc2c3bb72586ec32bd11b06
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-05-12 11:40:44 -0400

    style button

--- src/components/auth/auth0-login.js
+++ src/components/auth/auth0-login.js
@@ -4,7 +4,19 @@ import { useAuth0 } from '@auth0/auth0-react';
 const Auth0LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <button
+      style={{
+        height: '3vh',
+        borderRadius: '6px',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+      onClick={() => loginWithRedirect()}
+    >
+      Log In
+    </button>
+  );
 };
 
 export default Auth0LoginButton;
